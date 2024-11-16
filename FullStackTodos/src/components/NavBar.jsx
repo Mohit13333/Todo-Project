bb2c50a34bbeedf85afece4db36b7bd38afc2501
Mohit13333 <mohitkumar143680@gmail.com>
2024-11-16 20:32:49 +0530

    push

--- FullStackTodos/src/components/NavBar.jsx
+++ FullStackTodos/src/components/NavBar.jsx
@@ -9,12 +9,11 @@ const Navbar = () => {
     setIsOpen(!isOpen);
   };
   const { isLoggedIn, isLoading } = useAuth();
+
   if (isLoading) {
     return (
       <section className="flex items-center justify-center h-screen">
-        <div className="w-[100px] h-[100px] border-8 border-t-8 border-blue-600 border-solid rounded-full animate-spin">
-          Loading...
-        </div>
+        <div className="w-[100px] h-[100px] border-8 border-t-8 border-r-blue-600 border-t-green-600 border-l-rose-600 border-solid rounded-full animate-spin"></div>
       </section>
     );
   }
