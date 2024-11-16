38ac762c61ec5dd27369c077bec184bbb85656fd
Mohit13333 <mohitkumar143680@gmail.com>
2024-11-16 17:51:27 +0530

    auth was commented fix

--- Server-Side/Routers/todo.router.js
+++ Server-Side/Routers/todo.router.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
 import {todos,getTodos, deleteTodo, updateTodos, toggleTodoCheck, getSortedTodos} from "../controllers/todos.controller.js";
-// import authMiddleware from "../middlewares/auth.middleware.js";
+import authMiddleware from "../middlewares/auth.middleware.js";
 
 
 const router=Router();
 
-router.route('/post').post(todos);
-router.route('/getTodos').get(getTodos)
-router.route('/deleteTodos/:id').delete(deleteTodo);
-router.route('/updateTodos/:id').put(updateTodos);
-router.route("/toggleCheck/:id").put (toggleTodoCheck);
-router.route("/getSortedTodos").get (getSortedTodos);
+router.route('/post').post(authMiddleware,todos);
+router.route('/getTodos').get(authMiddleware,getTodos)
+router.route('/deleteTodos/:id').delete(authMiddleware,deleteTodo);
+router.route('/updateTodos/:id').put(authMiddleware,updateTodos);
+router.route("/toggleCheck/:id").put (authMiddleware,toggleTodoCheck);
+router.route("/getSortedTodos").get (authMiddleware,getSortedTodos);
 
 
 
