21b5afa201fb113bd757f65d14e1a3992ed2ee3a
Mohit13333 <mohitkumar143680@gmail.com>
2024-11-16 20:07:48 +0530

    push

--- Server-Side/Routers/contact.router.js
+++ Server-Side/Routers/contact.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import contactForm from "../Controllers/contactForm.controller.js";
+import contactForm from "../controllers/contactForm.controller.js";
 import authMiddleware from "../middlewares/auth.middleware.js";
 const router=Router();
 
