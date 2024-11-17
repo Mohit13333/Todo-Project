d23b9856e85c1f6388ce4b9f43ea895de625d1be
Mohit13333 <mohitkumar143680@gmail.com>
2024-11-18 01:07:57 +0530

    push

--- FullStackTodos/src/pages/Login.jsx
+++ FullStackTodos/src/pages/Login.jsx
@@ -71,13 +71,11 @@ const Login = () => {
                   className="w-[250px] h-[250px] md:w-[300px] md:h-[300px] rounded-lg shadow-md object-contain"
                 />
               </div>
-
               <div className="bg-gray-800 p-8 rounded-lg shadow-md border border-gray-700">
                 <h1 className="text-3xl font-bold text-center mb-6 relative">
                   Login
                   <span className="absolute left-1/2 bottom-[-10px] transform -translate-x-1/2 w-1/4 h-[3px] bg-blue-500"></span>
                 </h1>
-
                 <form onSubmit={handleSubmit} className="grid gap-6">
                   <div>
                     <label htmlFor="email" className="block font-semibold mb-2">
@@ -118,7 +116,15 @@ const Login = () => {
                   >
                     Login Now
                   </button>
-                  <p className="text-center">Don't have an Account?<NavLink className="mx-2 w-full text-blue-500 py-3 text-lg font-medium rounded-lg  hover:text-blue-700 transition-colors" to="/register">Register.</NavLink></p>
+                  <p className="text-center">
+                    Don't have an Account?
+                    <NavLink
+                      className="mx-2 w-full text-blue-500 py-3 text-lg font-medium rounded-lg  hover:text-blue-700 transition-colors"
+                      to="/register"
+                    >
+                      Register.
+                    </NavLink>
+                  </p>
                 </form>
               </div>
             </div>
